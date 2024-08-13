afed0b9f8a594cb7ad946547203cb828379b0aee
Michael-Chacon <74632437+Michael-Chacon@users.noreply.github.com>
2024-08-13 09:20:14 -0500

    feat: :sparkles: Fin del crud para la entidad Farmacy

--- src/main/java/com/examenSpring/web/controller/FarmacyController.java
+++ src/main/java/com/examenSpring/web/controller/FarmacyController.java
@@ -1,11 +1,13 @@
 package com.examenSpring.web.controller;
 
 import com.examenSpring.domain.service.farmacy.FarmacyService;
+import com.examenSpring.persistence.entity.Farmacy;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/farmacy")
@@ -16,7 +18,40 @@ public class FarmacyController {
 
     @GetMapping
     private ResponseEntity<?> findAll(){
-        return
+        return ResponseEntity.ok(service.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Farmacy> view(@PathVariable Long id) {
+        Optional<Farmacy> getFarmacy = service.findById(id);
+        if (getFarmacy.isPresent()) {
+            return ResponseEntity.ok(getFarmacy.orElseThrow());
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<Farmacy> save(@RequestBody Farmacy farmacy) {
+        Farmacy data = service.save(farmacy);
+        return ResponseEntity.status(HttpStatus.CREATED).body(data);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Farmacy> update(@PathVariable Long id, @RequestBody Farmacy farmacy) {
+        Optional<Farmacy> data = service.update(id, farmacy);
+        if (data.isPresent()) {
+            return ResponseEntity.status(HttpStatus.CREATED).body(data.orElseThrow());
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Farmacy> delete(@PathVariable Long id) {
+        Optional<Farmacy> farmacy = service.delete(id);
+        if (farmacy.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        }
+        return ResponseEntity.notFound().build();
     }
 
 }
---
+++ src/main/resources/import.sql
@@ -0,0 +1,10 @@
+INSERT INTO country (code_country, namecountry) VALUES ("col", "Colombia")
+INSERT INTO country (code_country, namecountry) VALUES ("al", "Alemania")
+
+INSERT INTO region (codereg, namereg, codecountry) VALUES ("ad","Andina", "col")
+INSERT INTO region (codereg, namereg, codecountry) VALUES ("car","Caribe", "col")
+
+INSERT INTO city (codecity, namecity, codereg) VALUES ("buc","Bucaramanga", "ad")
+
+
+
