7a435223c5dc4dc40728af8d66e2779a8268f64b
Michael-Chacon <michaelchacon02@gmail.com>
2024-08-13 06:15:30 -0500

    initial commit: :tada:

---
+++ src/main/java/com/examenSpring/ExamenSpringApplication.java
@@ -0,0 +1,13 @@
+package com.examenSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/examenSpring/ExamenSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examenSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
